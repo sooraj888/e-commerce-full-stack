3d855a7dd5e0d0aa67ea12550cf27d59c411bd9f
sooraj888 <soorajsagar888@gmail.com>
2025-07-19 00:32:49 +0530

    feat: add basic microservices with communication setup

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3002);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: { port: 3002, host: 'localhost' },
+    },
+  );
+  await app.listen();
 }
 bootstrap();
