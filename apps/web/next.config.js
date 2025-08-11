2d9f99021e9eaa9babdda57fc94bdb45e31eb226
sooraj888 <soorajsagar888@gmail.com>
2025-08-11 07:59:55 +0530

    build(next): make standalone output configurable via env variable

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,15 +3,21 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { composePlugins, withNx } = require('@nx/next');
 
+const isStandalone = process.env.NEXT_OUTPUT_MODE === 'standalone';
+
 /**
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
+
 const nextConfig = {
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
   nx: {},
-  output: 'standalone',
 };
+
+if (isStandalone) {
+  nextConfig['output'] = 'standalone';
+}
 
 const plugins = [
   // Add more Next.js plugins to this list if needed.
