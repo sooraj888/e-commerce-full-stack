c3a1d9f79b0d43ccb12cc270c26063cc7af13ce2
sooraj888 <soorajsagar888@gmail.com>
2025-08-14 10:09:02 +0530

    chore(api-gateway/env): rename PORT variable to APIGATEWAY_PORT

--- apps/api-gateway-e2e/src/support/global-setup.ts
+++ apps/api-gateway-e2e/src/support/global-setup.ts
@@ -8,7 +8,9 @@ module.exports = async function () {
   console.log('\nSetting up...\n');
 
   const host = process.env.HOST ?? 'localhost';
-  const port = process.env.PORT ? Number(process.env.PORT) : 3001;
+  const port = process.env.API_GATEWAY_PORT
+    ? Number(process.env.API_GATEWAY_PORT)
+    : 3001;
   await waitForPortOpen(port, { host });
 
   // Hint: Use `globalThis` to pass variables to global teardown.
