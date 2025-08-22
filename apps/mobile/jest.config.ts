8b7aced2233b0605354f11f8b5d43d5721113bc3
sooraj888 <soorajsagar888@gmail.com>
2025-08-22 16:26:22 +0530

    removed: react-test-renderer package

--- apps/mobile/jest.config.ts
+++ apps/mobile/jest.config.ts
@@ -1,6 +1,6 @@
 module.exports = {
   preset: 'react-native',
   transformIgnorePatterns: [
-    '../../node_modules/(?!(react-native|react|react-test-renderer|@react-native|@react-navigation|@react-native-community|react-clone-referenced-element)/)',
+    '../../node_modules/(?!(react-native|react|@react-native|@react-navigation|@react-native-community|react-clone-referenced-element)/)',
   ],
 };
